c7a9981a40fe69bfdd19e318a6306004638a20c4
RohanParmar2002 <rohanparmar143256@gmail.com>
2024-08-16 00:15:01 +0530

    initial

--- utils/index.ts
+++ utils/index.ts
@@ -46,7 +46,7 @@ export async function fetchCars(filters: FilterProps) {
 
   // Set the required headers for the API request
   const headers: HeadersInit = {
-    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+    "X-RapidAPI-Key": 'dfc86e73d8mshc76d6e943470cb2p1cb9b8jsn688894c8d88c' || "",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
